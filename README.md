d8ab7e0a4b8a1ce9c5b430e4fc2a43688610d460
Marc Cueto <gimpage@hotmail.com>
2024-05-28 17:29:29 -0400

    Create backend server

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 For dev challenge:
 https://devchallenges.io/challenge/my-task-board-app
+
+links and notes:
+https://devchallenges.io/editor/solution/23422
+
+to run with nodemon
+npm run dev
---
+++ backend/src/index.ts
@@ -0,0 +1,48 @@
+import express, { Request, Response } from 'express';
+import { getAllTasks, addTask, deleteTask } from './taskRepository';
+
+const app = express();
+
+// To parse JSON bodies
+app.use(express.json());
+
+// GET request
+app.get('/', (req, res) => {
+  try {
+    res.send(getAllTasks());
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Error occurred while getting tasks');
+  }
+});
+
+// POST request
+app.post('/', (req, res) => {
+  try {
+    const task = req.body;
+    const createdTask = addTask(task);
+    res.status(201).send(createdTask);
+    res.send(addTask(req.body));
+  } catch (error) {
+    res.status(500).send((error as Error).message);
+  }
+});
+
+// // PUT request
+// app.put("/", (req, res) => {
+//   res.send("PUT request to the homepage");
+// });
+
+// DELETE request
+app.delete('/', (req, res) => {
+  try {
+    console.log(req.body);
+    res.send(deleteTask(req.body.id));
+  } catch (error) {
+    res.status(500).send((error as Error).message);
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
